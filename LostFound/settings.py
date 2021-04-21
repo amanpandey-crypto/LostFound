3bca84aacac0d0a0261ba5971dc2df245711d276
Shubh Pathak <shubhpathak07@gmail.com>
2021-04-21 21:29:25 +0530

    .

--- LostFound/settings.py
+++ LostFound/settings.py
@@ -134,11 +134,16 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
+
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
